43a038243d57903957780f11d1f9ef7ecb19415b
syliesox <syliesox@gmail.com>
2019-06-22 02:40:02 -0400

    adding heroku set up connection

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -4,6 +4,35 @@ var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
+// Pull in required dependencies
+var mysql = require('mysql');
+
+// Create the MySQL connection object
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    // DB is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // DB is local on localhost
+    connection = mysql.createConnection({
+        port: 8889,
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'exampledb'
+    })
+};
+
+// Make the connection to MySQL
+connection.connect(function(err) {
+ if (err) {
+   console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+   return;
+ }
+ console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
 exports.signup = function (req, res) {
 
   res.render('signup');
