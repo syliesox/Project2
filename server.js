7adcb926fcc0d98a84328fca9c6e1f928a61a85e
syliesox <syliesox@gmail.com>
2019-06-22 10:38:59 -0400

    updated server.js file with PORT info

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var env        = require('dotenv').load();
 var exphbs     = require('express-handlebars');
 var path       = require("path");
 
+var PORT = process.env.PORT || 5000;
+
 //For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -63,7 +65,7 @@ console.log('Nice! Database looks fine')
 console.log(err,"Something went wrong with the Database Update!")
 });
 
-app.listen(5000, function(err){
+app.listen(PORT, function(err){
     if(!err)
     console.log("Site is live"); else console.log(err)
 
